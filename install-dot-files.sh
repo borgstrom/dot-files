bee7bf7bd9c25775a2aaca979c543cf5f64c7813
Evan Borgstrom <evan@borgstrom.ca>
2014-01-16 21:18:32 -0500

    Force install

--- install-dot-files.sh
+++ install-dot-files.sh
@@ -8,8 +8,7 @@ DOTFILES=$( cd $(dirname $0) ; pwd -P )
 for ent in .bashrc .screenrc .vim .vimrc; do
 	if [ -e ~/$ent ]; then
 		if [ ! -h ~/$ent ]; then
-			echo "Failed to install $ent, it already exists and is not a link"
-			continue
+			mv ~/$ent ~/$ent.before-dot-files
 		fi
 	fi
 
