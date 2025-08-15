69fb33820099f336b7e3dd6337f165fd935b2e2d
Evan Borgstrom <borgstrom@meta.com>
2025-08-15 11:18:33 -0700

    support fwdproxy-config

--- install-dot-files.sh
+++ install-dot-files.sh
@@ -12,5 +12,7 @@ for ent in .zshrc .zprofile .screenrc .tmux.conf .starship.toml; do
     ln -sf $DOTFILES/$ent ~/$ent
 done
 
+[[ -x $(which fwdproxy-config) ]] && FWDPROXY_CURL=$(fwdproxy-config curl)
+
 [[ ! -d $HOME/bin ]] && mkdir $HOME/bin
-BIN_DIR=$HOME/bin curl -sS https://starship.rs/install.sh | sh
+BIN_DIR=$HOME/bin curl $FWDPROXY_CURL -sS https://starship.rs/install.sh | sh
