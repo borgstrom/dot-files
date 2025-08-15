7671464c51e0c3c8b263ffdaf002adc4003393b5
Evan Borgstrom <borgstrom@meta.com>
2025-08-15 11:06:55 -0700

    Install starship when install dot-files

--- install-dot-files.sh
+++ install-dot-files.sh
@@ -2,15 +2,15 @@
 
 DOTFILES=$( cd $(dirname $0) ; pwd -P )
 
-git submodule init
-git submodule update --recursive
-
 for ent in .zshrc .zprofile .screenrc .tmux.conf .starship.toml; do
     if [ -e ~/$ent ]; then
-		if [ ! -h ~/$ent ]; then
-			mv ~/$ent ~/$ent.before-dot-files
-		fi
-	fi
+	    if [ ! -h ~/$ent ]; then
+		    mv ~/$ent ~/$ent.before-dot-files
+	    fi
+    fi
 
-	ln -sf $DOTFILES/$ent ~/$ent
+    ln -sf $DOTFILES/$ent ~/$ent
 done
+
+[[ ! -d $HOME/bin ]] && mkdir $HOME/bin
+BIN_DIR=$HOME/bin curl -sS https://starship.rs/install.sh | sh
